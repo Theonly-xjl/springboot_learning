64bf7cd1f8d38982c50390fa1552515e74dee82d
xjlInfo <u@udeMacBook-Pro.local>
2018-03-28 10:49:19 +0800

    返回 json

--- java-jkle-springboot-learning/src/main/java/com/jkle/springboot/JavaJkleSpringbootLearningApplication.java
+++ java-jkle-springboot-learning/src/main/java/com/jkle/springboot/JavaJkleSpringbootLearningApplication.java
@@ -2,8 +2,9 @@ package com.jkle.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class JavaJkleSpringbootLearningApplication {
 
 	public static void main(String[] args) {
